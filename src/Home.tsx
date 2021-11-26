a243837506a8fceabafc46dc261bfd23dc3edc1d
thryec <thryec@gmail.com>
2021-11-26 16:09:17 +0800

    added chain and network variables, created terra account

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,74 +2,58 @@ import { useWallet } from "@terra-money/wallet-provider";
 import { LCDClient } from "@terra-money/terra.js";
 import { useConnectedWallet } from "@terra-money/wallet-provider";
 import { useEffect, useState, useMemo } from "react";
+import {CHAINS, NETWORKS} from './variables'
+import { AnchorEarn, Account } from "@anchor-protocol/anchor-earn";
+
 import "./styles.css";
 
 const Home = () => {
-  const [connected, setConnected] = useState(false);
   const [balance, setBalance] = useState<null | string>();
   const connectedWallet = useConnectedWallet();
+  const { status, network } = useWallet();
 
-  const {
-    status,
-    network,
-    wallets,
-    availableConnectTypes,
-    availableInstallTypes,
-    availableConnections,
-    supportFeatures,
-    connect,
-    install,
-    disconnect,
-  } = useWallet();
+  const anchorEarn = new AnchorEarn({
+    chain: CHAINS.TERRA,
+    network: NETWORKS.BOMBAY_12,
+    mnemonic: '...',
+  });
+  const account = new Account(CHAINS.TERRA); 
 
-  const connectTerraStation = () => {
-    connect(availableConnections[0].type);
-    setConnected(true);
-  };
-
-  const disconnectTerraStation = () => {
-    disconnect();
-    setConnected(false);
-  };
 
   const lcd = useMemo(() => {
     if (!connectedWallet) {
       return null;
     }
-
     return new LCDClient({
       URL: connectedWallet.network.lcd,
       chainID: connectedWallet.network.chainID,
     });
   }, [connectedWallet]);
 
+  const fetchBalance = async () => {
+    if (connectedWallet && lcd) {
+      console.log("wallet: ", connectedWallet);
+      const res = await lcd.bank.balance(connectedWallet.walletAddress);
+      const coins = res[0].toString();
+      setBalance(coins);
+    } else {
+      setBalance(null);
+    }
+  };
 
   useEffect(() => {
-    const fetchBalance = async () => {
-      if (connectedWallet && lcd) {
-        console.log('wallet: ', connectedWallet)
-        const res = await lcd.bank.balance(connectedWallet.walletAddress)
-        const coins = res[0].toString() 
-        setBalance(coins)
-      } else {
-        setBalance(null);
-      }
-    }
-    fetchBalance()
+    fetchBalance();
+    console.log('account: ', account)
   }, [connectedWallet, lcd]);
+
 
 
   return (
     <div className="home">
-      <div>
         <p>Status: {status}</p>
         <p>Network Name: {network.name}</p>
         <p>Chain ID: {network.chainID}</p>
-      </div>
-
-      <div>
         <p>Wallet Balance: {balance}</p>
-      </div>
     </div>
   );
 };
---
+++ src/variables.ts
@@ -0,0 +1,9 @@
+export enum CHAINS {
+    TERRA = 'terra',
+  }
+
+  export enum NETWORKS {
+    COLUMBUS_5,
+    BOMBAY_12,
+  }
+  
